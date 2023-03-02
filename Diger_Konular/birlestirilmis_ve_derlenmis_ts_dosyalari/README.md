472383cfeb3b20a73df61c86c1198b9274091247
mrtgzn406 <mrtgzn406@gmail.com>
2023-03-02 22:53:45 +0300

    ts sayfalarını birleştirmek bu sürümde modül olan ve olmayan typescript sayfalarının nasıl tek bir .js sayfasına derleneceğini ile ilgili bir "Diger_Konular" klasörüne bir "README.md" dosyası ekledik. ayrıca bu sürümde modül olan ts sayfalarının nasıl minimized ve non-minimized şekilde tsk js dosyasına derleneceğini "webpack.config.js" dosyasına yorum olarak belirttik.

---
+++ Diger_Konular/birlestirilmis_ve_derlenmis_ts_dosyalari/README.md
@@ -0,0 +1,2 @@
+Bu klasördeki  modül ve modül olmayan ts sayfalarının nasıl birleştirilip tek sayfaya derlendiğini 
+öğrenmek için en dıştaki klasörde bulunan README.md sayfasını okuyun, orada bu konu anlatılıyor. 
--- Diger_Konular/birlestirilmis_ve_derlenmis_ts_dosyalari/modul_olan_ts_sayfalarini_birlestirme/webpack.config.js
+++ Diger_Konular/birlestirilmis_ve_derlenmis_ts_dosyalari/modul_olan_ts_sayfalarini_birlestirme/webpack.config.js
@@ -2,8 +2,14 @@ const path = require('path');
 
 module.exports = {
   entry: './src/ortak.ts',
-  mode: 'production',
+
+  // mode: 'development',
+  // minimize edilmemiş bir bundle.js sayfası istiyorsan bu yorumdaki kodları etkin yapman gerek ve bu yazının aşağısındaki "mode"ve "devtool" özelliklerini yoruma alman gerek. Yok eğer minimize edilmiş bir bundle.js sayfası istiyorsan bunun için sayfayı bu halde bırak, sayfa zaten şu an minmize edilmiş durumda.
+  
   devtool: 'source-map',
+  mode: 'production',
+  
+  
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
